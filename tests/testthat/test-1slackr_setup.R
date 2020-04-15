941e667796e35d0a017cfa55ce31ecc6450a26cf
Daniel P. Egan <dp.egan@gmail.com>
2020-04-15 10:13:48 -0400

    turn api key load into function

--- tests/testthat/test-1slackr_setup.R
+++ tests/testthat/test-1slackr_setup.R
@@ -1,12 +1,18 @@
-if(Sys.getenv("SLACK_API_TOKEN") == ""){
-  api_token <- yaml::read_yaml(file.path("inst", "exdata", "slackr_creds.yml"))$slackr$api_token
-  Sys.setenv("SLACK_API_TOKEN" = api_token)
-} else {
-  api_token <- Sys.getenv("SLACK_API_TOKEN")
+
+getAPIToken <- function(){
+  if(Sys.getenv("SLACK_API_TOKEN") == ""){
+    # This only works when you run it in development mode, not CHECK() or on Travis.
+    api_token <- yaml::read_yaml(file.path("exdata", "slackr_creds.yml"))$slackr$api_token
+    Sys.setenv("SLACK_API_TOKEN" = api_token)
+  } else {
+    api_token <- Sys.getenv("SLACK_API_TOKEN")
+  }
+  return(api_token)
 }
+api_token <- getAPIToken()
 
 test_that("slackr_setup works ", {
   expect_silent(slackr_setup(channel = "#publicchanneltest",
-               username = "slackr_bot",
-               api_token = api_token ))
+                             username = "slackr_bot",
+                             api_token = api_token ))
 })
